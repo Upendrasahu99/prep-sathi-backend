c6a456a3224e998e3c663e90ea3594281bf4f986
Upendra Sahu <upendrasahu1199@gmail.com>
2025-02-16 22:37:20 +0530

    feat: add global error middleware and setup cookie-parser, json, and urlencoded middleware

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import express from "express";
-
 import { PORT } from "./config/env.js";
-
+import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
@@ -9,8 +8,13 @@ import subjectRouter from "./routes/subject.routes.js";
 import questionRouter from "./routes/question.routes.js";
 import topicRouter from "./routes/topic.routes.js";
 import connectDB from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
@@ -18,6 +22,8 @@ app.use("/api/v1/subscriptions", subscriptionRouter);
 app.use("/api/v1/subjects", subjectRouter);
 app.use("/api/v1/questions", questionRouter);
 app.use("/api/v1/topics", topicRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Hello World");
