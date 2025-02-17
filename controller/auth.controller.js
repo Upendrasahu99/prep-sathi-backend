4804d77f0c171972904b04e1983ed0a1f24b7a0a
Upendra Sahu <upendrasahu1199@gmail.com>
2025-02-17 22:59:12 +0530

    feat: Implemented signin functionality

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -49,7 +49,38 @@ const signUp = async (req, res, next) => {
   }
 }
 const signIn = async (req, res, next) => {
-  //Implement sign in logic here
+  try{
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if(!user){
+      const error = new Error('User not found');
+      error.statusCode = 404;//No found
+      throw error;
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+      const error = new Error('Invalid password');
+      error.statusCode = 401; //Unauthorized
+      throw error;
+    }
+
+    const token = jwt.sign({userId: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRE_IN});
+
+    res.status(200).json({
+      success: true,
+      message: 'User signed in successfully',
+      data: {
+        user,
+        token
+      }
+    });
+  }catch(error){
+    next(error);
+  }
 }
 
 const signOut = async (req, res, next) => { 
