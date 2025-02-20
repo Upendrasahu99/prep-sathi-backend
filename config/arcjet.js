5cd5523e64e5aca515fce996fa4809b914ac8d78
Upendra Sahu <upendrasahu1199@gmail.com>
2025-02-20 07:41:44 +0530

    chore: update Arcjet configuration for testing rate limiter

--- config/arcjet.js
+++ config/arcjet.js
@@ -29,7 +29,7 @@ const aj = arcjet({
       mode: "LIVE",
       refillRate: 5, // Refill 5 tokens per interval
       interval: 10, // Refill every 10 seconds
-      capacity: 10, // Bucket capacity of 10 tokens
+      capacity: 5, // Bucket capacity of 10 tokens
     }),
   ],
 });
